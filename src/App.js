1d59c2ef6c76cf62b8842f7cddff86d0e95cb40d
Rafael Rocha <faelrribeiro3@gmail.com>
2023-09-21 22:17:36 -0300

    Site com requisição funcionando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [command, setCommand] = useState('');
+  const [output, setOutput] = useState('');
+
+  const executeCommand = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/exec-shell-command', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ command }),
+      });
+      const data = await response.json();
+      setOutput(data.stdout + '\n' + data.stderr);
+    } catch (error) {
+      setOutput('Erro: ' + error.message);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Executar Comando Shell</h1>
+      <input
+        type="text"
+        placeholder="Digite o comando"
+        value={command}
+        onChange={(e) => setCommand(e.target.value)}
+      />
+      <button onClick={executeCommand}>Executar Comando</button>
+      <div className="output">
+        <strong>Saída do Comando:</strong>
+        <pre>{output}</pre>
+      </div>
     </div>
   );
 }
